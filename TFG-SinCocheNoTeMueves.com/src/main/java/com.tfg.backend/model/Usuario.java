6b9e69a1b362ae13acfa56f48eeea5759a4c6193
Carlos Ródriguez Talegon <carlosrt123450@gmail.com>
2026-03-20 13:42:03 +0100

    Creacion del back

---
+++ TFG-SinCocheNoTeMueves.com/src/main/java/com.tfg.backend/model/Usuario.java
@@ -0,0 +1,97 @@
+package com.tfg.backend.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="usuarios")
+
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    @Column(unique=true)
+    private String dni;
+
+    @Column(unique=true)
+    private String email;
+
+    private String password;
+
+    private String telefono;
+
+    private String rol;
+
+    @OneToMany(mappedBy="propietario")
+    private List<Vehiculo> vehiculos;
+
+    public Usuario(){}
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getApellidos(){
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos){
+        this.apellidos=apellidos;
+    }
+
+    public String getDni(){
+        return dni;
+    }
+
+    public void setDni(String dni){
+        this.dni=dni;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public String getTelefono(){
+        return telefono;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono=telefono;
+    }
+
+    public String getRol(){
+        return rol;
+    }
+
+    public void setRol(String rol){
+        this.rol=rol;
+    }
+
+}

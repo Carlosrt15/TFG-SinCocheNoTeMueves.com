e157aa05e24a76c8d5d2d08f57f5253dd91846b1
MarioRC <mariorc2710@gmail.com>
2026-03-23 16:57:30 +0100

    .

--- TFG-SinCocheNoTeMueves.com/TFG-SinCocheNoTeMueves.com/TFG-SinCocheNoTeMueves.com/src/main/java/com.tfg.backend/repository/ContactoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tfg.backend.repository;
-
-import com.tfg.backend.model.Contacto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactoRepository
-        extends JpaRepository<Contacto,Long>{
-}
--- TFG-SinCocheNoTeMueves.com/TFG-SinCocheNoTeMueves.com/src/main/java/com.tfg.backend/repository/ResenaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tfg.backend.repository;
-
-import com.tfg.backend.model.Resena;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResenaRepository
-        extends JpaRepository<Resena,Long>{
-}
